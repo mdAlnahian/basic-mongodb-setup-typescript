6dc0f2a89c411ca1c7f55e5ae38d9200d57be789
mdAlnahian <muhammedalnahian@gmail.com>
2023-08-16 00:27:11 +0600

    tested the data using postman without frontend

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -1,26 +1,16 @@
+import { IUser } from "./user.interface";
 import User from "./user.model";
 
-export const createUserToDB = async () => {
+export const createUserToDB = async (payload: IUser): Promise<IUser> => {
   //completed creating a async function
 
-  const user = new User({
-    id: "100",
-    role: "student",
-    password: "passworddiyekiKorbajanainan",
-    name: {
-      firstName: "Ami",
-      middleName: "Al",
-      lastName: "Nahian asolei",
-    },
-    dateOfBirth: "12/12/12",
-    gender: "male",
-    email: "muhamdsfsdafsdmedalnahian@gmail.com",
-    contactNo: "01820124414",
-    emargencyContact: "01824423698",
-    presentAddress: "Oman ,biman ,bidesh",
-    permanentAddress: "Oman ,biman ,bideshdsfadf",
-  });
+  const user = new User(payload);
   //await here
   await user.save();
   return user;
+};
+
+export const getUsersFromDB = async (): Promise<IUser[]> => {
+  const users = await User.find();
+  return users;
 };
